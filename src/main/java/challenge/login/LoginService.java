1b700db1c244f29b2016c00efd3a3c7aff039c8c
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2026-07-05 19:39:32 +0200

    New Class LoginService - handles the sql query and returns a boolean

---
+++ src/main/java/challenge/login/LoginService.java
@@ -0,0 +1,41 @@
+package challenge.login;
+
+import database.DatabaseManager;
+import exceptions.LoginServiceException;
+
+import java.sql.SQLException;
+
+public class LoginService {
+    private final DatabaseManager manager;
+
+    public LoginService(DatabaseManager manager) {
+        this.manager = manager;
+    }
+
+    /// The `attemptLogin` method runs a deliberately vulnerable SQL query.<br>
+    /// String concatenation isn't sanitized, no `PreparedStatement` is used intentionally.<br>
+    /// Returns true or false and is used in `LoginHandler`
+    public boolean attemptLogin(String username, String password)
+            throws InterruptedException, LoginServiceException {
+
+        if (username == null || username.isBlank() ||
+            password == null || password.isBlank())
+        {
+            return false;
+        }
+
+        String query =
+                "SELECT * FROM users WHERE username = '" + username +
+                        "' AND password = '" + password + "'";
+
+        try (var conn = manager.getConnection();
+             var stmt = conn.createStatement();
+             var rs = stmt.executeQuery(query)) {
+            return rs.next();
+        } catch (SQLException e) {
+            throw new LoginServiceException(
+                    LoginService.class.getName() +
+                            ": SQL query failure.",e);
+        }
+    }
+}
