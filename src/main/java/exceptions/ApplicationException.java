93e4e60383c3af16ba3f5991dc5b60ac44f50c5e
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2026-07-07 19:52:49 +0200

    ApplicationException extends RuntimeException, now. Added a new constructor including only a String.

--- src/main/java/exceptions/ApplicationException.java
+++ src/main/java/exceptions/ApplicationException.java
@@ -2,9 +2,12 @@ package exceptions;
 
 import java.io.IOException;
 
-public class ApplicationException extends Throwable {
+public class ApplicationException extends RuntimeException {
     public ApplicationException(String message, IOException e) {
         super(message, e);
     }
 
+    public ApplicationException(String message) {
+        super(message);
+    }
 }
