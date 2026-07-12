66fcbe8e8df0e25346bdb9c7d29db64b2e51607a
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2026-07-12 12:01:29 +0200

    Added java doc to describe the class.

--- src/main/java/challenge/loginsqli/LoginSqliChallenge.java
+++ src/main/java/challenge/loginsqli/LoginSqliChallenge.java
@@ -5,14 +5,25 @@ import config.Configuration;
 import database.DatabaseManager;
 import database.SchemaInitializer;
 import exceptions.ChallengeException;
-import exceptions.LoginHandlerException;
-import exceptions.LoginServiceException;
+import exceptions.LoginSqliHandlerException;
+import exceptions.LoginSqliServiceException;
 import exceptions.SchemaInitializerException;
 import http.Route;
 
+/**
+ * Challenge module: {@code LoginSqliChallenge}
+ * Extends base class {@link Challenge} and does overload the {@code initialize()} method to set up a challenge related database table and dummy data.
+ * {@link DatabaseManager} is required to initialize.
+ */
 public class LoginSqliChallenge extends Challenge {
     private final DatabaseManager dbManager;
 
+    /**
+     * A Challenge object related to the SQL-Injection topic.
+     * @param config to load challenge specific configurations
+     * @param dbManager for initializing the database table
+     * @throws ChallengeException if an error occurs.
+     */
     public LoginSqliChallenge(Configuration config, DatabaseManager dbManager) throws ChallengeException {
         super(config);
         this.dbManager = dbManager;
@@ -25,8 +36,8 @@ public class LoginSqliChallenge extends Challenge {
         try {
             service = new LoginSqliService(dbManager);
             handler = new LoginSqliHandler(service, config());
-        } catch (LoginServiceException e) {
-            throw new LoginHandlerException(
+        } catch (LoginSqliServiceException e) {
+            throw new LoginSqliHandlerException(
                     getClass().getName() +
                             ": Failed to load LoginSqliService class.",e);
         }
