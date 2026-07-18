ced15d16041cfb98a0d00b27e9624624d6f0cd48
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2026-07-18 22:33:10 +0200

    Renamed IndexHandler
    A module integration named "Reconnaissance" required a class update of the index resource. It is now renamed to ChallengesHandler since the main purpose was to list all challenges. The new module will redirect after successfully submitting the flag.

--- src/main/java/http/ChallengesHandler.java
+++ src/main/java/http/ChallengesHandler.java
@@ -5,19 +5,19 @@ import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 
 /**
- * Serves the web application index page listing all available
+ * Serves the web application challenges page listing all available
  * challenges. Navigation provides a link to the
  * <p>
- * Handles {@code GET /index} and renders the static
- * {@code index.html} dashboard - a card holding all linking to
+ * Handles {@code GET /challenges} and renders the static
+ * {@code challenges.html} dashboard - a card holding all linking to
  * individual challenges.
  * </p>
  * <p>
- * Has no dependencies beyond {@link http.BaseHandler} - the index
+ * Has no dependencies beyond {@link http.BaseHandler} - the challenges
  * page is static and requires no configuration or rendering.
  * </p>
  */
-public class IndexHandler extends BaseHandler{
+public class ChallengesHandler extends BaseHandler{
 
     /**
      * Serves the web application index page.
@@ -28,7 +28,7 @@ public class IndexHandler extends BaseHandler{
      */
     @Override
     protected void doGet(HttpExchange exchange) throws IOException {
-        byte[] html = readResource("/static/index.html");
+        byte[] html = readResource("/static/challenges/challenges.html");
         sendResponse(exchange,200, TEXT_HTML, html);
     }
 }
